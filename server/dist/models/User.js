5cb6049bc81c2031098398590bfc0ac1dad7ba26
Sinan <muhammedsinan7749@gmail.com>
2024-08-16 10:01:56 +0530

    server package.json updated

--- server/dist/models/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-const userSchema = new Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-});
-const User = mongoose.model('User', userSchema);
-export default User;
-//# sourceMappingURL=User.js.map
