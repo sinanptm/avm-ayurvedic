8804c2fcef4caa66fd4e2d13ecee29dac9e13adc
Sinan <muhammedsinan7749@gmail.com>
2024-08-23 15:28:47 +0530

    Clean directories added

---
+++ resources.ts
@@ -0,0 +1,51 @@
+// ! TailWind
+// * npm install -D prettier prettier-plugin-tailwindcss
+
+// todo: Components
+// * tailwind components
+// * materail tailwind
+// * flowbite
+
+
+
+// ! Gradiant Generator 
+// * tailwindcss gradient gernorator 
+
+
+// * redix ui
+// * shadcn
+// * appwrite sms-database
+
+
+// * https://ui.aceternity.com/
+
+// *  https://github.com/MuhdHanish/carepulse_hcs
+
+
+//  Shadcn components to reduce the time spent building UI components.
+//  For managing forms, you can use TanStack Form or React Hook Form to reduce your workload.
+
+// * https://github.com/Mohammedshibilpv0/Freelance-X-server/tree/main
+
+// ! make sure to add caching in the server side responses
+
+
+// https://enhanced-button.vercel.app/ enhanced buttons
+
+
+
+// * you can create groute groups by (patient) === in the flloder name add paranthesis so we can access teh pages inside that without adding patent folder name 
+// * /register without,   /patient/register
+// * we can also add loyout to this route gorup folder 
+
+
+// *  can add metadata api to page.tsx to alter the laoyout metadata and change properties also we can added it nested pages and layouts
+// there is static and dynamic metadata
+// export const metadata:Metadata={
+//     title:{
+//         absolute:"",
+//         default:"",
+//         template:""
+//     },
+//     description:''
+// }
