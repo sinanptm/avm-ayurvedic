b963e7b31afec84ecfb9e17f990fab323cd12266
Sinan <muhammedsinan7749@gmail.com>
2024-08-23 15:27:12 +0530

    Clean directories added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,15 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.js';
+import { connectDB } from './config/connectDB';
 
-dotenv.config(); // Ensure this is at the top
+dotenv.config(); 
+
+const port = process.env.PORT||8080;
 
 const app = express();
 
-app.use(express.json());
-app.use('/api', authRoutes);  //post //api/login    //api/register
-
-// MongoDB connection
-const mongoUri = process.env.MONGO_URI;
-if (!mongoUri) {
-  console.error('MongoDB URI is not defined in environment variables.');
-  process.exit(1);
-}
-
-mongoose.connect(mongoUri)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
-// Start the server
-app.listen(process.env.PORT);
+connectDB().then(()=>{
+  app.listen(port,()=>{
+    console.log(`Server start listening on port: ${port}`);
+  });  
+})
