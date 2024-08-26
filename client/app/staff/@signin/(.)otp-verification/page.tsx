f1c9ae4cdd09d0030489133355613faae302f7e7
Sinan <muhammedsinan7749@gmail.com>
2024-08-26 12:28:17 +0530

    admin sign in page issue solved

--- client/app/staff/@signin/(.)otp-verification/page.tsx
+++ client/app/staff/@signin/(.)otp-verification/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+'use client';
+import { FormEvent } from 'react'
 import OtpVerificationModel from '@/components/models/OtpVerificationModel'
 
-const OptInterceptor = () => <OtpVerificationModel returnRoute='/staff/' />
-   
+const OptInterceptor = () => {
+    const handleResend = ()=>{
+
+    }
+    const handleVerify = (e:FormEvent)=>{
+        handleVerify
+    }
+
+    return <OtpVerificationModel handleResend={handleResend} handleVerify={handleResend} timer={15} key={12} returnRoute='/staff/' />
+}
 
 export default OptInterceptor
