95cc28991469794e13cb8d80b57a466ab75559d4
Sinan <muhammedsinan7749@gmail.com>
2024-08-28 20:17:11 +0530

    auth state added;

--- client/lib/features/authSlice.ts
+++ client/lib/features/authSlice.ts
@@ -1,21 +1,67 @@
-import { createSlice } from "@reduxjs/toolkit";
+"use client";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
+interface AuthState {
+   patientToken: string;
+   doctorToken: string;
+   adminToken: string;
+}
+
+const persistedAuthState: AuthState =
+   typeof window !== "undefined" ? JSON.parse(localStorage.getItem("auth") || "{}") : {};
+   
+const initialState: AuthState = {
+   patientToken: persistedAuthState.patientToken || "",
+   doctorToken: persistedAuthState.doctorToken || "",
+   adminToken: persistedAuthState.adminToken || "",
+};
+
+interface SetCredentialsPayload {
+   accessToken: string;
+   type: "patient" | "admin" | "doctor";
+}
+
+interface LogOutPayload {
+   type: "patient" | "admin" | "doctor";
+}
 
 const authSlice = createSlice({
    name: "auth",
-   initialState: { token: "" },
+   initialState,
    reducers: {
-      setCredentials: (state, action: { payload: { accessToken: string }; type: string }) => {
-         const { accessToken } = action.payload;
-         state.token = accessToken;
+      setCredentials: (state, action: PayloadAction<SetCredentialsPayload>) => {
+         const { accessToken, type } = action.payload;
+         if (type === "patient") {
+            state.patientToken = accessToken;
+         } else if (type === "doctor") {
+            state.doctorToken = accessToken;
+         } else if (type === "admin") {
+            state.adminToken = accessToken;
+         }
+         if (typeof window !== "undefined") {
+            localStorage.setItem("auth", JSON.stringify(state));
+         }
       },
-      logOut: (state, action) => {
-         state.token = "";
+      logOut: (state, action: PayloadAction<LogOutPayload>) => {
+         const { type } = action.payload;
+         if (type === "patient") {
+            state.patientToken = "";
+         } else if (type === "doctor") {
+            state.doctorToken = "";
+         } else if (type === "admin") {
+            state.adminToken = "";
+         }
+         if (typeof window !== "undefined") {
+            localStorage.setItem("auth", JSON.stringify(state));
+         }
       },
    },
 });
 
 export const { setCredentials, logOut } = authSlice.actions;
 
-export const selectCurrentToken = (state: RootState) => state.auth.token;
+export const selectPatientToken = (state: RootState) => state.auth.patientToken;
+export const selectAdminToken = (state: RootState) => state.auth.adminToken;
+export const selectDoctorToken = (state: RootState) => state.auth.doctorToken;
+
 export default authSlice.reducer;
--- client/lib/store.ts
+++ client/lib/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "@/lib/features/authSlice";
-import localStorageMiddleware from "./middlewares/localStorageMiddleware";
 export const makeStore = () => {
    return configureStore({
       reducer: {
          auth: authSlice,
       },
-      middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(localStorageMiddleware),
       devTools: process.env.NEXT_PUBLIC_ENV !== "production",
    });
 };
