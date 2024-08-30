d44354b5b70b46d2305de9a999c48fb368349df7
Sinan <muhammedsinan7749@gmail.com>
2024-08-30 16:30:07 +0530

    issue solved

--- client/app/(patient)/signin/(.)otp-verification/page.tsx
+++ client/app/(patient)/signin/(.)otp-verification/page.tsx
@@ -1,18 +1,13 @@
 "use client";
 import { FormEvent } from "react";
 import OtpVerificationModel from "@/components/models/OtpVerificationModel";
-import { useAuth } from "@/hooks/useAuth";
-import { notFound } from "next/navigation";
 
 const OptInterceptor = () => {
    const handleVerify = (e: FormEvent) => {
       e.preventDefault();
    };
    const handleResend = () => {};
-   const {patientToken} = useAuth();
-   if(patientToken){
-      notFound();
-   }
+
    return (
          <OtpVerificationModel
             returnRoute="/signin"
