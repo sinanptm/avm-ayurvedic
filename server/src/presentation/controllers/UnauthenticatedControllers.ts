73bebd5370b38c4b966bdb4a0b40e5816a21eb9b
Sinan <muhammedsinan7749@gmail.com>
2024-09-12 11:31:21 +0530

    limit updated

--- server/src/presentation/controllers/UnauthenticatedControllers.ts
+++ server/src/presentation/controllers/UnauthenticatedControllers.ts
@@ -7,12 +7,12 @@ export default class UnathenicatedControllers {
 
     async getDoctors(req: Request, res: Response, next: NextFunction) {
         try {
-            let offset = parseInt(req.query.offset as string);
-            let limit = parseInt(req.query.limit as string);
+            // let offset = parseInt(req.query.offset as string);
+            // let limit = parseInt(req.query.limit as string);
 
-            offset = isNaN(offset) || offset < 0 ? 0 : offset;
-            limit = isNaN(limit) || limit < 0 ? 10 : Math.min(limit, 100);
-            res.status(StatusCode.Success).json(await this.unauthenticatedUseCase.getDoctors(offset, limit))
+            // offset = isNaN(offset) || offset < 0 ? 0 : offset;
+            // limit = isNaN(limit) || limit < 0 ? 10 : Math.min(limit, 100);
+            res.status(StatusCode.Success).json(await this.unauthenticatedUseCase.getDoctors(0, 100))
         } catch (error) {
             next(error)
         }
--- server/src/use_case/UnauthenticatedUseCases.ts
+++ server/src/use_case/UnauthenticatedUseCases.ts
@@ -4,15 +4,13 @@ import { PaginatedResult } from "../types";
 
 
 export default class UnauthenticatedUseCases {
-    constructor(private doctorRepository: IDoctorRepository) { }
-
-
+    constructor(private doctorRepository: IDoctorRepository) {}
 
     async getDoctors(offset: number, limit: number): Promise<PaginatedResult<IDoctor>> {
         const isVerified = true;
         const isBlocked = false
         const data = await this.doctorRepository.findMany(offset, limit, isVerified, isBlocked);
-        data.items = data.items.filter(item=>item.role!=='admin')
+        data.items = data.items.filter(item => item.role !== 'admin')
         return data;
     }
 }
