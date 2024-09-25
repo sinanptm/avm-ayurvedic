fd54eca0b3d8ae071239e5841195ab60b8612f40
Sinan <muhammedsinan7749@gmail.com>
2024-09-25 06:42:03 +0530

    🖼️ doctor side chat completed

--- client/components/page-components/chat/getUserData.ts
+++ client/components/page-components/chat/getUserData.ts
@@ -1,16 +1,18 @@
-export const getSenderData = (sender: "doctor" | "patient", doctorData: string, patientData: string):string => {
+export const getSenderData = (sender: "doctor" | "patient", doctorData: string, patientData: string): string => {
   if (sender === 'patient') {
     return patientData
-  } else if(sender === 'doctor') {
-    return doctorData ;
+  } else if (sender === 'doctor') {
+    return doctorData;
   }
+  return 'Unknown'
 }
 
 
-export const getReceiverData = (sender: "doctor" | "patient", doctorData: string, patientData: string):string => {
+export const getReceiverData = (sender: "doctor" | "patient", doctorData: string, patientData: string): string => {
   if (sender === 'patient') {
     return doctorData
-  } else if(sender === 'doctor') {
-    return patientData ;
+  } else if (sender === 'doctor') {
+    return patientData;
   }
+  return 'Unknown'
 }
