23184c2d7e6b7590dd09b3abdf6f824bb220c2b5
Sinan <muhammedsinan7749@gmail.com>
2024-09-28 17:31:11 +0530

    🔄 slot time filtering only for today added

--- server/src/use_case/slot/GetSlotUseCase.ts
+++ server/src/use_case/slot/GetSlotUseCase.ts
@@ -9,7 +9,7 @@ export default class GetSlotUseCase {
       private slotRepository: ISlotRepository,
       private appointmentRepository: IAppointmentRepository,
       private validatorService: IValidatorService
-   ) {}
+   ) { }
 
    async getAllSlots(doctorId: string): Promise<ISlot[] | null> {
       this.validatorService.validateIdFormat(doctorId);
@@ -22,39 +22,47 @@ export default class GetSlotUseCase {
       return await this.slotRepository.findManyByDay(doctorId, day);
    }
 
-   async getSlotsByDate(doctorId: string, date: string): Promise<ISlot[] | []> {
-      this.validatorService.validateIdFormat(doctorId);
-      this.validatorService.validateDateFormat(date);
-
-      const dateAfterOneHour = addHours(new Date(),1);
-
-      const appointments = await this.appointmentRepository.findManyByDateAndDoctorId(date, doctorId);
-      const slotIds = appointments?.map((el) => el.slotId!) || [];
-      const day = this.getDayFromDate(date);
-      const slots = await this.slotRepository.findManyNotInSlotIds(doctorId, day, slotIds);
-
-      // filtering the slots that are before the dateAfterOneHour
-      return slots ? slots.filter(slot=>{
-         const startTime = this.parseTimeStringToDate(slot.startTime!);
-         return startTime > dateAfterOneHour;
-      }) : []
-   }
-
    private getDayFromDate(date: string): Days {
       const dayOfWeek = new Date(date).getUTCDay();
       const dayNames = Object.values(Days);
       return dayNames[dayOfWeek] as Days;
    }
 
-   private parseTimeStringToDate (timeString: string): Date {
+   async getSlotsByDate(doctorId: string, date: string): Promise<ISlot[] | []> {
+      this.validatorService.validateIdFormat(doctorId);
+      this.validatorService.validateDateFormat(date);
+
+      const dateAfterOneHour = addHours(new Date(), 1);
+      const appointments = await this.appointmentRepository.findManyByDateAndDoctorId(date, doctorId);
+      const slotIds = appointments?.map((el) => el.slotId!) || [];
+      const day = this.getDayFromDate(date);
+      const slots = await this.slotRepository.findManyNotInSlotIds(doctorId, day, slotIds);
+
+      const isToday = new Date(date).toDateString() === new Date().toDateString();
+
+      // Filter today's slots based on time (slots after the current time + 1 hour)
+      return slots
+         ? slots.filter(slot => {
+            if (isToday) {
+               const startTime = this.parseTimeStringToDate(slot.startTime!);
+               return startTime > dateAfterOneHour;
+            }
+            return true; // Return all slots for future dates
+         })
+         : [];
+   }
+
+
+
+   private parseTimeStringToDate(timeString: string): Date {
       const [time, modifier] = timeString.split(' ');
       let [hours, minutes] = time.split(':').map(Number);
-    
+
       if (modifier === 'PM' && hours < 12) hours += 12;
       if (modifier === 'AM' && hours === 12) hours = 0;
-      
+
       const date = new Date();
-      date.setHours(hours, minutes, 0, 0); 
+      date.setHours(hours, minutes, 0, 0);
       return date;
-    };
+   };
 }
