49a192fe8e5adca163f31ad8fdb34528036d3b1a
Sinan <muhammedsinan7749@gmail.com>
2024-09-29 06:20:51 +0530

    server sockeit updated

--- server/src/presentation/socket/index.ts
+++ server/src/presentation/socket/index.ts
@@ -1,7 +1,8 @@
 import { Server } from "socket.io";
-import { Server as HTTPServer } from "http"; // Type from the HTTP server
+import { Server as HTTPServer } from "http"; 
 import { CLIENT_URL } from "../../config/env";
 import logger from "../../utils/logger";
+import manageVideoSection from "./videoSection";
 
 const initializeSocket = (server: HTTPServer) => {
     const io = new Server(server, {
@@ -12,35 +13,9 @@ const initializeSocket = (server: HTTPServer) => {
         },
     });
 
-    // Listen for new connections
     io.on("connection", (socket) => {
         console.log(`User connected: ${socket.id}`);
-
-        // Handle joining a room
-        socket.on("join-room", (roomId) => {
-            socket.join(roomId);
-            logger.info(`User ${socket.id} joined room ${roomId}`);
-        });
-
-        // Handle offering WebRTC signal (from one client to another)
-        socket.on("offer", (offer, roomId) => {
-            socket.to(roomId).emit("offer", offer);
-        });
-
-        // Handle answering WebRTC offer
-        socket.on("answer", (answer, roomId) => {
-            socket.to(roomId).emit("answer", answer);
-        });
-
-        // Handle ICE candidates for WebRTC
-        socket.on("ice-candidate", (candidate, roomId) => {
-            socket.to(roomId).emit("ice-candidate", candidate);
-        });
-
-        // Handle disconnection
-        socket.on("disconnect", () => {
-            logger.warn(`User disconnected: ${socket.id}`);
-        });
+        manageVideoSection(socket);
     });
 };
 
