77861e8b49fe34cb560c5903f999bc849030b49f
Sinan <muhammedsinan7749@gmail.com>
2024-10-04 17:46:57 +0530

    🚧 notification socket manager completed

--- server/src/presentation/socket/notification/NotificationSocketManager.ts
+++ server/src/presentation/socket/notification/NotificationSocketManager.ts
@@ -1,21 +1,26 @@
-import ITokenService from "../../../domain/interface/services/ITokenService";
-import NotificationUseCase from "../../../use_case/notification/NotificationUseCae";
 import { Namespace, Server, Socket } from "socket.io";
-import logger from "../../../utils/logger";
+import NotificationUseCase from "../../../use_case/notification/NotificationUseCae";
+import ITokenService from "../../../domain/interface/services/ITokenService";
+import CustomError from "../../../domain/entities/CustomError";
 import { TokenPayload, UserRole } from "../../../types";
+import logger from "../../../utils/logger";
 
 export default class NotificationSocketManager {
-    private io: Namespace
+    private io: Namespace;
+
     constructor(
         io: Server,
         private notificationUseCase: NotificationUseCase,
         private tokenService: ITokenService
     ) {
         this.io = io.of("/notification");
-        this.initializeNotificationNamespace()
+        this.setupMiddleware();
+        this.initializeNotificationNamespace();
+    }
+
+    private setupMiddleware() {
         this.io.use((socket, next) => {
             const token = socket.handshake.auth.token;
-
             if (!token) {
                 logger.warn(`Socket ${socket.id} attempted connection without token`);
                 return next(new Error("Authentication error"));
@@ -31,23 +36,62 @@ export default class NotificationSocketManager {
         });
     }
 
-
     private initializeNotificationNamespace() {
-        this.io.on("connection", (socket) => {
+        this.io.on("connection", (socket: Socket) => {
             const user = socket.data.user as TokenPayload;
-
             logger.info(`User ${user.email} connected to /notification`);
 
-            this.handleEvents(socket);
+            this.initializeEvents(socket);
 
             socket.on("disconnect", (reason) => {
                 logger.info(`User ${user.email} disconnected: ${reason}`);
             });
-
         });
     }
 
-    private handleEvents(socket:Socket){
-        
+    private initializeEvents(socket: Socket) {
+        socket.on("getNotifications",
+            async () => this.handleErrors(socket, this.getNotifications(socket))
+        );
+        socket.on("clearNotification",
+            async (notificationId: string) => this.handleErrors(socket, this.clearOne(socket, notificationId))
+        );
+        socket.on("clearAllNotifications",
+            async (notificationIds: string[]) => this.handleErrors(socket, this.clearAll(socket, notificationIds))
+        );
+    }
+
+    private async getNotifications(socket: Socket) {
+        const user = socket.data.user as TokenPayload;
+        let notifications;
+        if (user.role === UserRole.Patient) {
+            notifications = await this.notificationUseCase.getAllPatient(user.id);
+        } else if (user.role === UserRole.Doctor) {
+            notifications = await this.notificationUseCase.getAllDoctor(user.id);
+        }
+        socket.emit("notifications", notifications);
+    }
+
+    private async clearOne(socket: Socket, notificationId: string) {
+        await this.notificationUseCase.clearOn(notificationId);
+        socket.emit("notificationCleared", notificationId);
+    }
+
+    private async clearAll(socket: Socket, notificationIds: string[]) {
+        await this.notificationUseCase.clearAll(notificationIds);
+        socket.emit("notificationsCleared", notificationIds);
+    }
+
+    private async handleErrors(socket: Socket, handler: Promise<void>) {
+        try {
+            await handler;
+        } catch (error) {
+            if (error instanceof CustomError) {
+                socket.emit("error", error.message);
+            } else {
+                socket.emit("error", "An unexpected error occurred");
+                logger.error(error);
+            }
+        }
     }
 }
