d474fa9c4e66e5fd3fd56dfed73d8953809f9b95
Sinan <muhammedsinan7749@gmail.com>
2024-10-12 12:15:52 +0530

    spinner import solved

--- client/components/page-components/chat/ChatSection.tsx
+++ client/components/page-components/chat/ChatSection.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input"
 import { ArrowLeft, Send, AlertCircle, Smile } from "lucide-react"
 import { IChat, IMessage } from "@/types/entities"
 import { useRouter } from "next/navigation"
-import  Spinner from "@/components/skeletons/Spinners";
+import  Spinner from "@/components/skeletons/Spinner";
 import { getSenderData } from "./getUserData"
 import dynamic from 'next/dynamic'
 import { EmojiClickData, Theme } from 'emoji-picker-react'
